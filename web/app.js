ccf46cf541e5231592adc27668d5bc57bb403a27
tubi55 <tubi55@nate.com>
2026-09-01 12:18:42 +0900

    특정 고객정보 확인 함수 추가

--- web/app.js
+++ web/app.js
@@ -6,10 +6,11 @@ const headers = {
 };
 const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
 
-//현재 프로젝트에서 headers가 필요한 이유는 현재 페이지의 담당 관리자가 "나"라는 것을 증명하기 위함
-//로그인하려고 만든게 아님
-//추후 외부 서비스 연결시 담당자가 나임을 구분시키기 위한 정보값
+// ==================================
+//  회원정보 요청 함수 등록 및 호출
+// ==================================
 
 // 고객정보 get 방식으로 요청하는 함수 정의
 const getCustomer = async () => {
@@ -25,7 +26,7 @@ btn1.addEventListener("click", async () => {
 });
 
 // ==================================
-// 상품정보 요청 함수 등록 및 호출
+//  상품정보 요청 함수 등록 및 호출
 // ==================================
 // 고객정보 get 방식으로 요청하는 함수 정의
 const getProduct = async () => {
@@ -37,4 +38,19 @@ const getProduct = async () => {
 btn2.addEventListener("click", async () => {
   const result = await getProduct();
   console.log(result.content);
+});
+
+// ==========================================================
+//  인자로 특정 고객 아이디를 받아서 고객정보 요청 함수 등록 및 호출
+// ==========================================================
+
+const getCustomerInfo = async (id) => {
+  const res = await fetch(`/api/customers/${id}`, headers);
+  if (!res.ok) throw new Error();
+  return res.json();
+};
+
+btn3.addEventListener("click", async () => {
+  const result = await getCustomerInfo("C001");
+  console.log(result);
 });
