746899d930d9a103e46e4e82a526d5aed29617fe
tubi55 <tubi55@nate.com>
2026-09-01 14:07:13 +0900

    이벤트 발생시 사용자 정보 출력

--- web/app.js
+++ web/app.js
@@ -17,21 +17,26 @@ const section = document.querySelector("#frame");
 const getCustomer = async () => {
   const res = await fetch("/api/customers", headers);
   if (!res.ok) throw new Error();
-  return res.json();
+  const result = await res.json();
+  return result;
 };
 
 // 상품정보 확인 함수
 const getProduct = async () => {
   const res = await fetch("/api/products", headers);
   if (!res.ok) throw new Error();
-  return res.json();
+  // 응답객체를 json형태로 변환하는 작업도 시간이 걸리는 작업이기 때문에 await로 promise객체 정보값이 완료될때까지 홀딩(동기화)
+  const result = await res.json();
+  return result;
 };
 
 // 특정 고객의 종합적인 구매 정보 확인함수
 const getCustomerInfo = async (id) => {
   const res = await fetch(`/api/customers/${id}`, headers);
   if (!res.ok) throw new Error();
-  return res.json();
+  const result = await res.json();
+  console.log(result);
+  return result;
 };
 
 // 리스트로 출력할 고객정보 배열로 반환함수
@@ -43,13 +48,11 @@ const getCustomerList = async () => {
 
   // 배열을 반복돌면서 각각의 데이터와 순번 출력
   lists.forEach((data, index) => {
-    console.log(data);
     tags += `
-      <button>${data.name}</button>
+      <button ${(onclick = () => getCustomerInfo(data.customer_id))}>${data.name}</button>
     `;
   });
 
-  console.log(tags);
   frame.innerHTML = tags;
 };
 
