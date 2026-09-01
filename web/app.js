c44903b8f06203009af1ad1dc7687a20a22e8c05
tubi55 <tubi55@nate.com>
2026-09-01 12:35:49 +0900

    사용자 목록을 반복하며 리스트로 출력

--- web/app.js
+++ web/app.js
@@ -7,50 +7,50 @@ const headers = {
 const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 const btn3 = document.querySelector("#btn3");
+const section = document.querySelector("#frame");
 
 // ==================================
 //  회원정보 요청 함수 등록 및 호출
 // ==================================
 
-// 고객정보 get 방식으로 요청하는 함수 정의
+// 고객정보 확인함수
 const getCustomer = async () => {
   const res = await fetch("/api/customers", headers);
   if (!res.ok) throw new Error();
   return res.json();
 };
 
-// 첫번째 버튼 클릭시 고객정보 반환함수 호출구문
-btn1.addEventListener("click", async () => {
-  const result = await getCustomer();
-  console.log(result);
-});
-
-// ==================================
-//  상품정보 요청 함수 등록 및 호출
-// ==================================
-// 고객정보 get 방식으로 요청하는 함수 정의
+// 상품정보 확인 함수
 const getProduct = async () => {
   const res = await fetch("/api/products", headers);
   if (!res.ok) throw new Error();
   return res.json();
 };
 
-btn2.addEventListener("click", async () => {
-  const result = await getProduct();
-  console.log(result.content);
-});
-
-// ==========================================================
-//  인자로 특정 고객 아이디를 받아서 고객정보 요청 함수 등록 및 호출
-// ==========================================================
-
+// 특정 고객의 종합적인 구매 정보 확인함수
 const getCustomerInfo = async (id) => {
   const res = await fetch(`/api/customers/${id}`, headers);
   if (!res.ok) throw new Error();
   return res.json();
 };
 
-btn3.addEventListener("click", async () => {
-  const result = await getCustomerInfo("C001");
-  console.log(result);
-});
+// 리스트로 출력할 고객정보 배열로 반환함수
+
+const getCustomerList = async () => {
+  const lists = await getCustomer();
+
+  let tags = "";
+
+  // 배열을 반복돌면서 각각의 데이터와 순번 출력
+  lists.forEach((data, index) => {
+    console.log(data);
+    tags += `
+      <button>${data.name}</button>
+    `;
+  });
+
+  console.log(tags);
+  frame.innerHTML = tags;
+};
+
+getCustomerList();
